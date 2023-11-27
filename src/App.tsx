fb5981ca0b83b1c8cd397f2d33788be10c6b6797
aloha <2240379431@qq.com>
2023-11-27 23:43:10 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,192 +1,205 @@
-import './App.css';
-import { bitable, Selection, ITable, IAttachmentField } from "@lark-base-open/js-sdk";
-import { Button, Divider, Image, Space, Typography, Toast, Spin, Form, Card, Tooltip } from '@douyinfe/semi-ui';
-import { useState, useEffect, useRef, useCallback, } from 'react';
+import { useState, useEffect, useRef, useCallback } from 'react';
+import { bitable, ITable, IAttachmentField } from "@lark-base-open/js-sdk";
+import { Button, Divider, Image, Space, Typography, Toast, Spin, Form, Card, Tooltip, Popconfirm,Banner } from '@douyinfe/semi-ui';
 import imageCompression from 'browser-image-compression';
-import { cloneDeep } from "lodash"
-import { IconInfoCircle } from "@douyinfe/semi-icons"
-import { formatFileSize } from './image';
+import { cloneDeep } from "lodash";
+import { IconInfoCircle } from "@douyinfe/semi-icons";
+import { debounce } from 'lodash';
+import './App.css';
 
-// todo: 把form去掉,赋值慢,在获取field优化下
-
+// 定义类型
 type ImageItem = {
   url: string;
   file: File;
   name: string;
   size: number;
-  type: string; // mime
+  type: string;
   token: string;
   timeStamp: number;
-  recordId: string
-  fieldId: string
-}
+};
+
 
 
 
 type ImageRecordList = {
-  fieldId: string
-  recordId: string
-  images: ImageItem[]
-}
+  fieldId: string;
+  recordId: string;
+  images: ImageItem[];
+};
 
 
 
+const formatFileSize = (fileSizeInBytes: number): string => {
+  if (fileSizeInBytes < 1024) {
+    return fileSizeInBytes + " B";
+  } else if (fileSizeInBytes < 1024 * 1024) {
+    return (fileSizeInBytes / 1024).toFixed(2) + " KB";
+  } else if (fileSizeInBytes < 1024 * 1024 * 1024) {
+    return (fileSizeInBytes / (1024 * 1024)).toFixed(2) + " MB";
+  } else {
+    return (fileSizeInBytes / (1024 * 1024 * 1024)).toFixed(2) + " GB";
+  }
+};
 
-type Selected = {
-  field: IAttachmentField
-  selection: Selection
-}
 
-
-type SelectionChangeOptions = {
-  data: Selection
-}
 
 
 export default function App() {
 
+  const [imageRecordList, setImageRecordList] = useState<ImageRecordList[]>([]); // 
+  const [compressNum, setCompressNum] = useState<number>(60);
+  const [pattern, setPattern] = useState<string>('cell');
+  const [loading, setLoading] = useState<boolean>(false);
 
-  // 图片列表
-  const [imageRecordList, setImageRecordList] = useState<ImageRecordList[]>([])
+  const tableRef = useRef<ITable | null>(null);
+  const selectionFieldRef = useRef<IAttachmentField | null>(null);
+  const lastFieldIdRef = useRef<string | null>(null);
 
-  // 压缩比
-  const [compressNum, setCompressNum] = useState<number>(10)
 
-  const [pattern, setPattern] = useState<string>('cell')
-
-  const [loading, setLoading] = useState<boolean>(false)
-  // 当前table
-  const tableRef = useRef<ITable | null>()
-  // 选中的field
-  const selectionFieldRef = useRef<IAttachmentField>()
+  const isFetchingRef = useRef(false); // 用于标记是否正在获取数据
 
 
 
-
-
-
-  // 单击单元格获取图片
-  const getData = async (event?: SelectionChangeOptions) => {
-
-
-    setLoading(true)
-    try {
-
-      tableRef.current = await bitable.base.getActiveTable();
-
-      let list = []
-      let imgs: ImageRecordList[] = []
-
-      let selection = await bitable.base.getSelection()
-
-
-      const field = await tableRef.current?.getField<IAttachmentField>(selection?.fieldId as string) as IAttachmentField
-
-      selectionFieldRef.current = field
-
-
-      console.log(pattern)
-
-
-      console.log(selection.fieldId)
-      // console.log()
-
-
-      //  根绝类型获取数据
-      if (pattern === 'cell') {
-
-        const arr = await field.getValue(selection?.recordId as string)
-
-        list.push({
-          recordId: selection.recordId as string,
-          fieldId: selection.fieldId as string,
-          images: arr
-        })
-
-
-      } else if (pattern === 'field') {
-
-        const recordList = await tableRef.current.getRecordList();
-
-        for (const record of recordList) {
-          const cell = await record.getCellByField(selection.fieldId as string);
-          const val = await cell.getValue();
-          if (val) {
-            list.push({
-              recordId: record.id,
-              fieldId: selection.fieldId as string,
-              images: val
-            })
-          }
-        }
-      }
-
-      console.log(list)
-
-      if (list && list.length > 0) {
-
-        for (let record of list) {
-          const arr: ImageItem[] = []
-
-          for (let img of record.images) {
-            if (img.type.split('/')[0] === "image") {
-              const url = await tableRef.current?.getAttachmentUrl(img.token) as string ?? ''
-              const file = await fetchImageAsFile(url, img.name)
-              const item = { ...img, file, url, recordId: selection.recordId, field: selection.fieldId }
-              arr.push(item)
-            }
-          }
-
-          imgs.push({
-            recordId: record.recordId,
-            fieldId: record.fieldId,
-            images: arr
-          })
-
-
-        }
-      } else {
-        imgs = []
-      }
-
-      setImageRecordList(imgs)
-      setLoading(false)
-
-    } catch (e) {
-      setLoading(false)
-    }
-
-  }
-
-  const funRef = useRef(getData)
-
+  const patternRef = useRef(pattern); // 使用 useRef 来跟踪 pattern 的最新值
 
 
 
   useEffect(() => {
-    init()
+    const init = async () => {
+      try {
+        bitable.base.onSelectionChange(
+          debounce(handleSelectionChange, 100) // 使用 lodash 的 debounce 方法
+        );
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    init();
   }, []);
 
 
-  // useEffect(() => {
-  //   bitable.base.getSelection().then((res: Selection) => {
-  //     getData({ data: res })
-  //   })
-
-
-  //   console.log(pattern)
-
-  // }, [pattern])
-
-
-  const init = async () => {
-    try {
-      bitable.base.onSelectionChange(()=>funRef.current());
+  const handleSelectionChange = useCallback(async () => {
+    if (isFetchingRef.current) {
+      console.log("Previous fetch in progress, cancelling...");
+      return; // 如果正在获取数据，则取消
     }
-    catch (e) {
-      console.log(e)
+    console.log(patternRef.current);
+    const currentSelection = await bitable.base.getSelection();
+    console.log(currentSelection);
+    if (currentSelection.fieldId && currentSelection.recordId) {
+      if (patternRef.current === 'field' && lastFieldIdRef.current === currentSelection?.fieldId) {
+        return;
+      }
+      lastFieldIdRef.current = currentSelection?.fieldId || null;
+      getData();
+    } else {
+      // setImageRecordList([]);
+      // setLoading(false);
     }
+  }, []);
+
+
+
+  const onPatternChange = async (pa: string) => {
+    patternRef.current = pa; // 每次 pattern 更新时，更新其在 useRef 中的值
+    console.log(patternRef.current)
+
+    await getData()
   }
+
+
+
+  const getData = async () => {
+    const currentPattern = patternRef.current; // 使用 useRef 中的最新值
+
+
+    setLoading(true);
+
+    try {
+      tableRef.current = await bitable.base.getActiveTable();
+      if (!tableRef.current) {
+        setImageRecordList([])
+        setLoading(false);
+
+        return;
+      }
+
+      const selection = await bitable.base.getSelection();
+      if (!selection) {
+        setImageRecordList([])
+        setLoading(false);
+        return;
+      }
+
+      const field = await tableRef.current.getField<IAttachmentField>(selection.fieldId as string);
+      if (!field) {
+        setImageRecordList([])
+        setLoading(false);
+        return;
+      }
+
+
+      selectionFieldRef.current = field;
+
+      let recordList = [];
+
+      console.log(currentPattern)
+
+
+      if (currentPattern === 'cell') {
+        recordList.push(selection.recordId);
+      } else {
+        const allRecords = await tableRef.current.getRecordList();
+        for (const record of allRecords) {
+          recordList.push(record.id)
+        }
+      }
+      console.log(recordList)
+
+
+
+      const images = await Promise.all(recordList.map(async recordId => {
+        console.log(recordId)
+        let imgs: ImageItem[] | [] = []
+
+        const imgItems = await field.getValue(recordId as string);
+
+        console.log(imgItems)
+
+        if (imgItems) {
+
+          imgs = await Promise.all(imgItems.filter(img => img.type.startsWith('image')).map(async img => {
+            const url = await tableRef.current?.getAttachmentUrl(img.token) as string;
+            return {
+              ...img,
+              url,
+              file: await fetchImageAsFile(url, img.name)
+            };
+          }))
+        }
+
+
+
+
+        return {
+          recordId: recordId as string,
+          fieldId: selection.fieldId as string,
+          images: imgs
+        };
+      }));
+
+      console.log(images.filter(item => item.images.length > 0))
+      console.log(images)
+
+
+      setImageRecordList(images.filter(item => item.images.length > 0));
+    } catch (e) {
+      console.error(e);
+    } finally {
+      isFetchingRef.current = false; // 完成获取数据
+      setLoading(false);
+    }
+  };
 
 
 
@@ -233,7 +246,6 @@ export default function App() {
   const confirmCompress = async () => {
     setLoading(true)
     try {
-      // const res = await selectField?.field.setValue(selectField.selection.recordId as string, fileList)
 
       const resArr: boolean[] = []
 
@@ -256,9 +268,9 @@ export default function App() {
 
   const ImageListEl = () => {
 
-    return <Spin spinning={loading}>
+    return <>
       {
-        loading ? null : imageRecordList.map(item => {
+        imageRecordList.map(item => {
           return item.images.map(img => {
             return <Space vertical key={img.name}>
               <Image
@@ -273,71 +285,64 @@ export default function App() {
             </Space>
           })
         })
-
       }
-    </Spin>
+    </>
 
 
   }
-
-  const judgeTip = (type: string) => {
-
-    return {
-      cell: '请先选择图片所在单元格',
-      field: '请先选择图片所在列'
-    }[type]
-
-  }
-
-
 
   return (
-    <main className="main" style={{}}>
+    <Spin spinning={loading}>
+        {/* <Banner 
+            type="info"
+            description="使用过程中请保持单元"
+        /> */}
+      <main className="main" >
+        <Card  >
 
+          <Form onValueChange={values => {
+            setPattern(values.pattern as string)
+            setCompressNum(values.compressNum as number)
 
-      <Card  >
+          }} initValues={{ compressNum, pattern }}>
+            <Form.Section text={'压缩设置'}>
+              <Form.Slider max={100} min={1} field='compressNum' label={
+                <Space>
+                  <span> 压缩粒度:{compressNum}</span>
+                  <Tooltip content={'压缩的粒度越大,压缩的图片越小'}>
+                    <IconInfoCircle />
 
-        <Form onValueChange={values => {
-          console.log(values)
-          setPattern(values.pattern as string)
-          setCompressNum(values.compressNum as number)
+                  </Tooltip>
+                </Space>
+              } />
 
-        }} initValues={{ compressNum, pattern }}>
-          <Form.Section text={'压缩设置'}>
-            <Form.Slider max={100} min={1} field='compressNum' label={
-              <Space>
-                <span> 压缩粒度:{compressNum}</span>
-                <Tooltip content={'压缩的粒度越大,压缩的图片越小'}>
-                  <IconInfoCircle />
+              <Form.Select field='pattern' label="压缩模式" onChange={e => onPatternChange(e as string)}>
+                <Form.Select.Option value="cell">压缩当前选中单元格</Form.Select.Option>
+                <Form.Select.Option value="field">压缩整列</Form.Select.Option>
+              </Form.Select>
 
-                </Tooltip>
-              </Space>
-            } />
+            </Form.Section>
+          </Form>
 
+          <Space>
+            <Button disabled={imageRecordList.length === 0 || loading} theme='solid' onClick={handleCompress} >压缩</Button>
+            <Popconfirm
+              title="确定要批量更新吗？"
+              onConfirm={confirmCompress}
+            >
+              <Button disabled={imageRecordList.length === 0 || loading} theme='solid' >应用</Button>
+            </Popconfirm>
 
-            <Form.Select field='pattern' label="压缩模式">
-              <Form.Select.Option value="cell">压缩当前选中单元格</Form.Select.Option>
-              <Form.Select.Option value="field">压缩整列</Form.Select.Option>
-            </Form.Select>
+          </Space>
+        </Card>
 
-          </Form.Section>
-        </Form>
+        <Divider margin='12px' />
+        {
+          imageRecordList.length > 0 ? <ImageListEl /> : `请先选择图片所在${pattern === 'cell' ? '单元格' : '列'}`
+        }
 
-        <Space>
-
-          <Button disabled={imageRecordList.length === 0 || loading} theme='solid' onClick={handleCompress} >压缩</Button>
-          <Button disabled={imageRecordList.length === 0 || loading} theme='solid' onClick={confirmCompress} >应用</Button>
-        </Space>
-      </Card>
-
-      <Divider margin='12px' />
-
-
-      {
-        imageRecordList.length > 0 ? <ImageListEl /> : judgeTip(pattern)
-      }
-
-    </main>
+      </main>
+    </Spin>
   )
 }
 
